700f8b7744bea1444de9980a65078e8213f847ae
Carlos LLanos <cllanoscor@gmail.com>
2025-06-13 19:56:44 +0000

    pull

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,19 +1,37 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/img/recetea-logo.png";
+
 
 export const Navbar = () => {
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-light px-4 py-2">
+      <div className="container-fluid">
+
+        {/* Logo a la izquierda */}
+        <a className="navbar-brand" href="/">
+          <img src={logo} alt="Logo" className="logo-navbar" />
+        </a>
+
+        <form className="d-flex" role="search">
+          <input className="form-control me-2 r-10" type="search" placeholder="Search recipes, diets and more" aria-label="Search" />
+          <button className="btn btn-outline-primary" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+
+        {/* Botón hamburguesa para colapsar en móvil */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarButtons">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Contenido colapsable */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarButtons">
+          <div className="d-flex gap-2">
+            <a href="/login" className="btn btn-primary">Login</a>
+            <a href="/Register" className="btn btn-primary">Register</a>
+          </div>
+        </div>
+
+      </div>
+    </nav>
+  );
 };
