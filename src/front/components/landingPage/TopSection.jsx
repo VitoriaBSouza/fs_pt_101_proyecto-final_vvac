485dd676482f9ecbf58c1acb9565cf1f48a6fef9
Carlos LLanos <cllanoscor@gmail.com>
2025-06-17 14:00:41 +0000

    pull

--- src/front/components/landingPage/TopSection.jsx
+++ src/front/components/landingPage/TopSection.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import imgTop from '../assets/img/img-Top.jpg';
 
 export const TopSection = () => {
   return (
-    <div className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: "url('/ruta/a/tu/imagen.jpg')" }}>
+    <div
+      style={{
+        backgroundImage: `url(${imgTop})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '400px'
+      }}
+    >
+      {/* Contenido aquí */}
+    </div>
+  );
+};
       <div className="absolute right-10 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-70 p-6 rounded shadow-lg max-w-sm">
-        <h1 className="text-3xl font-bold mb-2">Título Impactante</h1>
+        
         <p className="text-gray-700">Aquí va tu mensaje principal o llamado a la acción.</p>
       </div>
     </div>
   );
 };
-
