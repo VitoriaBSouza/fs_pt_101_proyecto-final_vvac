e15ef3af4bcd5400415399645399399b9ee958f9
Carlos LLanos <cllanoscor@gmail.com>
2025-06-20 12:12:26 +0000

    bottomsection

--- src/front/components/BottomSection.jsx
+++ src/front/components/BottomSection.jsx
@@ -3,28 +3,94 @@ import React from 'react';
 
 export const BottomSection = () => {
   return (
-    <section className="bg-light py-5">
-      <div className="container">
+    <section className="bg-light py-5" >
+      <div className="container " >
         <h2 className="text-center fw-bold mb-5 display-6">Explore Our Meal Options</h2>
         <div className="row g-4">
-          {diets.map((diet, index) => (
-            <div className="col-md-4" key={index}>
-              <div className="card h-100 shadow-sm">
+            <div className="col-md-4" >
+              <div className="card h-100 shadow-sm " style={{border: "2px solid #ca3e49"}}>
                 <img
-                  src={diet.image}
-                  alt={diet.title}
+                  src="src/front/assets/img/VeganDiet.jpg"
+                  alt=""
                   className="card-img-top"
-                  style={{ height: '180px', objectFit: 'cover' }}
+                  style={{ height: '180px', objectFit: 'cover', border: "2px solid rgb(0, 0, 0)"}}
                 />
-                <div className="card-body">
-                  <h5 className="card-title"></h5>
-                  <ul className="list-unstyled small mb-0">
-                    
-                  </ul>
+                <div className="card_bottom">
+                  <h2 className="card-title"> VEGAN DIET </h2>
+                  <p></p>
                 </div>
               </div>
             </div>
-          ))}
+            <div className="col-md-4">
+              <div className="card h-100 shadow-sm" style={{border: "2px solid #ca3e49"}}>
+                <img
+                  src="src/front/assets/img/GlutenFree.jpg"
+                  alt=""
+                  className="card-img-top"
+                  style={{ height: '180px', objectFit: 'cover', border: "2px solid rgb(0, 0, 0)" }}
+                />
+                <div className="card_bottom">
+                  <h5 className="card-title">GLUTEN FREE</h5>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card h-100 shadow-sm" style={{border: "2px solid #ca3e49"}}>
+                <img
+                  src="src/front/assets/img/Proteic.jpg"
+                  alt=""
+                  className="card-img-top"
+                  style={{ height: '180px', objectFit: 'cover', border: "2px solid rgb(0, 0, 0)" }}
+                />
+                <div className="card_bottom">
+                  <h5 className="card-title">HIGH PROTEIC</h5>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card h-100 shadow-sm" style={{border: "2px solid #ca3e49"}}>
+                <img
+                  src="src/front/assets/img/low-calorie.jpg"
+                  alt=""
+                  className="card-img-top"
+                  style={{ height: '180px', objectFit: 'cover', border: "2px solid rgb(0, 0, 0)" }}
+                />
+                <div className="card_bottom">
+                  <h5 className="card-title">LOW CALORIES</h5>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card h-100 shadow-sm" style={{border: "2px solid #ca3e49"}}>
+                <img
+                  src="src/front/assets/img/FastRecipes.jpg"
+                  alt=""
+                  className="card-img-top"
+                  style={{ height: '180px', objectFit: 'cover', border: "2px solid rgb(0, 0, 0)" }}
+                />
+                <div className="card_bottom">
+                  <h5 className="card-title">FAST RECIPES</h5>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card h-100 shadow-sm" style={{border: "2px solid #ca3e49"}}>
+                <img
+                  src="src/front/assets/img/CheatMeals.jpg"
+                  alt=""
+                  className="card-img-top"
+                  style={{ height: '180px', objectFit: 'cover', border: "2px solid rgb(0, 0, 0)" }}
+                />
+                <div className="card_bottom">
+                  <h5 className="card-title">CHEAT MEALS</h5>
+                  <p></p>
+                </div>
+              </div>
+            </div>
         </div>
       </div>
     </section>
