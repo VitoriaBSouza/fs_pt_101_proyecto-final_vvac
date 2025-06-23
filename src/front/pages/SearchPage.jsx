dfcd0b129d58eb5f9ed5bb7a4e2f380e0947cbfd
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-06-23 20:14:38 +0000

    third commit

--- src/front/pages/SearchPage.jsx
+++ src/front/pages/SearchPage.jsx
@@ -1,16 +1,24 @@
+// src/front/pages/SearchPage.jsx
 import React from "react";
-import { Navbar } from "react-bootstrap";
 import { TurnHome } from "../components/buttons/TurnHome.jsx";
 import { SearchFilter } from "../components/SearchFilter.jsx";
+import { LinksMenu } from "../components/LinksMenu.jsx";
 
 export const SearchPage = () => {
     return (
         <div className="container-fluid">
             <TurnHome />
-            <SearchFilter />
+            <div className="row">
+                {/* Menú lateral izquierdo */}
+                <div className="col-md-2 p-3">
+                    <LinksMenu />
+                </div>
+
+                {/* SearchFilter contiene filtros + vista de recetas */}
+                <div className="col-md-10 p-3">
+                    <SearchFilter />
+                </div>
+            </div>
         </div>
     );
 };
-
-
-
