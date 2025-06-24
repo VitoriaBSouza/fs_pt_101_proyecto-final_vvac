2d723c888d8fd157a223a9e43a2561a50f391cb9
Carlos LLanos <cllanoscor@gmail.com>
2025-06-24 23:22:26 +0000

    ajustes

--- src/front/components/BottomSection.jsx
+++ src/front/components/BottomSection.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const BottomSection = () => {
   return (
 
-    <div className="container_bottom  w-100 ">
+    <div className="container_bottom m-0 w-100 ">
       <h2 className="title_Recipe_bottom text-center fw-bold mb-0 text-dark">Explore Our Meal Options</h2>
       <div className="scroll-bottom-row">
         <div className="card card_scroll_bottom h-100 shadow-sm " style={{ border: "10px solid #ca3e49" }}>
@@ -15,7 +15,7 @@ export const BottomSection = () => {
             style={{ height: '180px', objectFit: 'cover'}}
           />
           <div className="card_bottom w-100 bg-opacity-50">
-            <h5 className="bottom_title text-center text-light fs-2 "> VEGAN DIET </h5>
+            <h5 className="bottom_title text-center text-dark  fs-2 "> VEGAN DIET </h5>
             <p className='text-center fs-5 fw-bold'> Recipes made on plant-based ingredients.</p>
           </div>
         </div>
@@ -28,7 +28,7 @@ export const BottomSection = () => {
             style={{ height: '180px', objectFit: 'cover' }}
           />
           <div className="card_bottom w-100 bg-opacity-50">
-            <h5 className="bottom_title text-center text-light fs-2 ">GLUTEN FREE</h5>
+            <h5 className="bottom_title text-center text-dark fs-2 ">GLUTEN FREE</h5>
             <p className='text-center fs-5  fw-bold'>Caelic recipes without wheat, barley, or rye.</p>
           </div>
         </div>
@@ -40,7 +40,7 @@ export const BottomSection = () => {
             style={{ height: '180px', objectFit: 'cover'}}
           />
           <div className="card_bottom w-100 bg-opacity-50">
-            <h5 className="bottom_title text-center text-light fs-2 ">HIGH PROTEIC</h5>
+            <h5 className="bottom_title text-center text-dark fs-2 ">HIGH PROTEIC</h5>
             <p className='text-center fs-5 fw-bold'>Meals rich in protein for muscle and energy.</p>
           </div>
         </div>
@@ -56,7 +56,7 @@ export const BottomSection = () => {
             style={{ height: '180px', objectFit: 'cover'}}
           />
           <div className="card_bottom w-100 bg-opacity-50">
-            <h5 className="bottom_title text-center text-light fs-2 ">LOW CALORIES</h5>
+            <h5 className="bottom_title text-center text-dark fs-2 ">LOW CALORIES</h5>
             <p className='text-center fs-5 fw-bold'> Light recipes to help you stay in shape.</p>
           </div>
         </div>
@@ -68,8 +68,8 @@ export const BottomSection = () => {
             style={{ height: '180px', objectFit: 'cover'}}
           />
           <div className="card_bottom w-100 bg-opacity-50">
-            <h5 className="bottom_title text-center text-light fs-2 ">FAST RECIPES</h5>
-            <p className='text-center fs-5 fw-bold'>Quick meals in 30 minutes or less.</p>
+            <h5 className="bottom_title text-center text-dark fs-2 ">FAST RECIPES</h5>
+            <p className='text-center fs-5 fw-bold'>Taste & GO! Quick recipes in 30 minutes or less.</p>
           </div>
         </div>
         <div className="card card_scroll_bottom h-100 shadow-sm" style={{ border: "10px solid #ca3e49" }}>
@@ -80,7 +80,7 @@ export const BottomSection = () => {
             style={{ height: '180px', objectFit: 'cover'}}
           />
           <div className="card_bottom w-100 bg-opacity-50">
-            <h5 className="bottom_title text-center text-light fs-2  ">CHEAT MEALS</h5>
+            <h5 className="bottom_title text-center text-dark fs-2  ">CHEAT MEALS</h5>
             <p className='text-center fs-5 fw-bold'>Indulgent treats to enjoy without guilt.</p>
           </div>
         </div>
