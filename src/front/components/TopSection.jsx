82a457f8d4061337bb13092f0866fa5bd197ef43
Carlos LLanos <cllanoscor@gmail.com>
2025-06-25 14:35:30 +0000

    a main

--- src/front/components/TopSection.jsx
+++ src/front/components/TopSection.jsx
@@ -12,17 +12,17 @@ export const TopSection = () => {
                 />
 
                 <div className="position-absolute top-50 end-0 translate-middle-y 
-                text-white p-4 rounded-2 me-5 me-md-5 top_intro_text">
-
-                    <h1 className="fw-bold text-warning display-5">Get to know Recetea</h1>
-                    <h5 className="mb-0">Collect all your recipes in one place.</h5>
-                    <h5 className="mb-0">Plan your menu and get your shopping list ready.</h5>
-                    <h5 className="mb-0">Discover flavors you've never tried.</h5>
-                    <h5 className="mb-0">Choose to eat healthier every day.</h5>
-
+                text-white  top_intro_text p-3 me-5 me-md-5  lh-1.5 rounded-2">
+                    <div className= "position-absolute">
+                        <h1 className="fw-bold text-warning text-center  display-5">Explore Recetea</h1>
+                        <h5 className="mb-0">Collect all your recipes in one place.</h5>
+                        <h5 className="mb-0">Plan your menu and get your shopping list ready.</h5>
+                        <h5 className="mb-0">Discover flavors you've never tried.</h5>
+                        <h5 className="mb-0">Choose to eat healthier every day.</h5>
+                    </div>
                 </div>
-
             </div>
         </div>
+
     );
 };
