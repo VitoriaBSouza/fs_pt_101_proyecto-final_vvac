ea76b2e1a8a75e50d8ddf279a746fee0163d2302
Javier Seiglie <96433186+jseiglie@users.noreply.github.com>
2025-06-25 12:16:06 +0000

    fix

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -4,19 +4,18 @@ import logo from "../assets/img/recetea-logo.png";
  export const Footer = () => {
   return (
     <footer className="container-fluid  bg-light text-dark mt-0 pt-0 pb-2">
-      <div className="row align-items-center">
-        <div className="col-md-6 d-flex align-items-center">
+      <div className="align-items-center  d-flex justify-content-between">
+        <div className=" align-items-center">
           <img src={logo} alt="Logo" className='logo-footer' />
+        </div>
+    
+        <div className="align-items-center">
+          <p className="footer_contact fs-5">Contact Us: receteaapi@gmail.com</p>
         </div>
       </div>
 
       <hr className="border-secondary my-2" />
 
-      <div className="row align-items-center">
-        <div className="col-md-6 d-flex align-items-center">
-          <p className="footer_contact fs-3">Contact Us: receteaapi@gmail.com</p>
-        </div>
-      </div>
 
       <div className="text-center pe-4">
         <p className="rights_reserved fs-5 fw-bold">&copy; 2025 Recetea - All rights reserved</p>
